738367a054fe7af337041e08d5355f54bb11e5ca
dernDren161 <kitsarpgyawali@gmail.com>
2022-08-24 07:25:20 +0530

    System Design

--- Amazon-Geeks/LinkedList/ReverseALinkedList.cpp
+++ Amazon-Geeks/LinkedList/ReverseALinkedList.cpp
@@ -17,3 +17,19 @@ struct Node* reverseList(struct Node *head)
 
     return head;
 }
+
+// Iterative Solution
+
+Node* reverse(Node* node)
+    {
+        if (node == NULL)
+            return NULL;
+        if (node->next == NULL) {
+            head = node;
+            return node;
+        }
+        Node* node1 = reverse(node->next);
+        node1->next = node;
+        node->next = NULL;
+        return node;
+    }
--- test2.cpp
+++ test2.cpp
@@ -1,28 +1,114 @@
-// Ding it using a DLL
+// Vehicle and its inherited classes.
+public enum VehicleSize { Motorcycle, Compact,Large }
 
-struct Node{
-	int val;
-	Node* next; Node* prev;
+public abstract class Vehicle
+{
+	protected ArrayList<ParkingSpot> parkingSpots =
+						new ArrayList<ParkingSpot>();
+	protected String licensePlate;
+	protected int spotsNeeded;
+	protected VehicleSize size;
 
-	Node(int val){
-		this->val = val;
-		next = NULL; prev = NULL;
+	public int getSpotsNeeded()
+	{
+		return spotsNeeded;
 	}
-};
-int size = 0; Node* head; Node* mid;
-// f<------->r
-void push(int x){
-	
+	public VehicleSize getSize()
+	{
+		return size;
+	}
+
+	/* Park vehicle in this spot (among others,
+		potentially) */
+	public void parkinSpot(ParkingSpot s)
+	{
+		parkingSpots.add(s);
+	}
+
+
+	/* Remove vehicle from spot, and notify spot
+		that it's gone */
+	public void clearSpots() { ... }
+
+	/* Checks if the spot is big enough for the
+		vehicle (and is available).
+		This * compares the SIZE only.It does not
+		check if it has enough spots. */
+	public abstract boolean canFitinSpot(ParkingSpot spot);
 }
 
-int pop(){
+public class Bus extends Vehicle
+{
+	public Bus()
+	{
+		spotsNeeded = 5;
+		size = VehicleSize.Large;
+	}
 
+	/* Checks if the spot is a Large. Doesn't check
+	num of spots */
+	public boolean canFitinSpot(ParkingSpot spot)
+	{... }
 }
 
-int findMid(){
+public class Car extends Vehicle
+{
+	public Car()
+	{
+		spotsNeeded = 1;
+		size = VehicleSize.Compact;
+	}
 
+	/* Checks if the spot is a Compact or a Large. */
+	public boolean canFitinSpot(ParkingSpot spot)
+	{ ... }
 }
 
-void deleteMid{
+public class Motorcycle extends Vehicle
+{
+	public Motorcycle()
+	{
+		spotsNeeded = 1;
+		size = VehicleSize.Motorcycle;
+	}
+	public boolean canFitinSpot(ParkingSpot spot)
+	{ ... }
+}
+---------------------------------------------------------------------------------------
 
+public class ParkingSpot
+{
+	private Vehicle vehicle;
+	private VehicleSize spotSize;
+	private int row;
+	private int spotNumber;
+	private Level level;
+
+	public ParkingSpot(Level lvl, int r, int n,
+						VehicleSize s)
+	{ ... }
+
+	public boolean isAvailable()
+	{
+		return vehicle == null;
+	}
+
+	/* Check if the spot is big enough and is available */
+	public boolean canFitVehicle(Vehicle vehicle) { ... }
+
+	/* Park vehicle in this spot. */
+	public boolean park(Vehicle v) {..}
+
+	public int getRow()
+	{
+		return row;
+	}
+	public int getSpotNumber()
+	{
+		return spotNumber;
+	}
+
+	/* Remove vehicle from spot, and notify
+	level that a new spot is available */
+	public void removeVehicle() { ... }
 }
